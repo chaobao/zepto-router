5bfb6e3d47f6ebb0f333fbd7b193e4f5374892eb
zquancai <zquancai@126.com>
2015-05-22 16:35:43 +0800

    完成node服务端路由

--- server/server.js
+++ server/server.js
@@ -2,27 +2,66 @@
  * Created by zqc on 2015/5/20.
  */
 
-console.log('dddd');
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path');
 
-var http = require('http');
+var config = {
+    version: '0.0.1',
+    port: 3000,
+    static_script: './static/script',
+    unique_html: './example/plugin-test.html'
+};
 
-http.get('http://newdata.3g.cn/jsonInterface/index.php/Nba/Tuku/getTukuHeader?callback=jQuery19106984910371247679_1432103039210&_=1432103039211', function (res) {
-    console.log('STATUS:' + res.statusCode);
-    console.log('HEADERS:' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data',function(chunk){
-        eval(chunk);
+var routes = {
+    '/static/script': '/static/script',
+    '/man/info': '/man/info',
+    '/man/setting': '/man/setting',
+    '/homework/unhand': '/homework/unhand',
+    '/homework/feedback': '/homework/feedback'
+};
+
+function checkUrl (url) {
+    return routes[url] ? true : false;
+}
+
+function readStaticFile (fpath, res) {
+    fs.readFile(fpath, function(err, buf){
+        if (err){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end("file " + fpath + " not readable " + err);
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': buf.length
+            });
+            res.write(buf);
+            res.end();
+        }
     });
-}).on('error',function(e){
-    console.log(e.message);
-});
-
-function jQuery19106984910371247679_1432103039210 (data) {
-    console.log(data);
 }
 
 http.createServer(function(req, res){
-    console.log(req.url);
-    res.writeHeader('200',{'Content-Type':'text/plain'});
-    res.end(JSON.stringify({a:'dddd'}));
-}).listen(3000);
+    if(req.url.indexOf(config.static_script) !== -1){
+        readStaticFile(req.url, res);
+    }
+    else if(checkUrl(req.url)) {
+        fs.readFile(config.unique_html, function (err, buf) {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end("file " + config.unique_html + " not readable " + err);
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    'Content-Length': buf.length
+                });
+                res.write(buf);
+                res.end();
+            }
+        });
+    }
+    else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("file " + req.url + " not found ");
+    }
+}).listen(config.port);
--- server/server2.js
+++ server/server2.js
@@ -1,3 +1,19 @@
 /**
  * Created by 郑权才 on 2015/5/20.
  */
+
+
+//http.get('http://newdata.3g.cn/jsonInterface/index.php/Nba/Tuku/getTukuHeader?callback=jQuery19106984910371247679_1432103039210&_=1432103039211', function (res) {
+//    console.log('STATUS:' + res.statusCode);
+//    console.log('HEADERS:' + JSON.stringify(res.headers));
+//    res.setEncoding('utf8');
+//    res.on('data',function(chunk){
+//        eval(chunk);
+//    });
+//}).on('error',function(e){
+//    console.log(e.message);
+//});
+//
+//function jQuery19106984910371247679_1432103039210 (data) {
+//    console.log(data);
+//}
